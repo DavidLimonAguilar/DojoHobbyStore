a75654b1b9037f75e08239bdc376fb2ac27e49c8
David Limón Aguilar <davidlimonaguilardev@gmail.com>
2024-03-31 22:40:36 -0600

    Se hace responsiva

--- DojoHobbyStorePage/src/components/manga.tsx
+++ DojoHobbyStorePage/src/components/manga.tsx
@@ -7,9 +7,11 @@ export const Manga = () => {
             <div className="manga-main">
                 <h2>MANGA</h2>
                 <div className="manga-text">
-                <p>Es la palabra japonesa para designar a las historietas en general. Fuera de japón se utiliza tanto para referirse a las historiatas de origen japonés como al estilo de dibujo utilizado en estas. Abarca una extensa variedad de géneros
-                    y llega a publicos diversos.Tenemos los títulos más famosos y grandiosos del manga como: Dragon Ball, Naruto, One Piece, Death Note de la mano  de los distribuidores oficiales como lo son PANINI  COMICS MEXICO y KAMITE.</p>
-                    <img src={chivi1} alt="" />
+                    <p>Es la palabra japonesa para designar a las historietas en general. Fuera de japón se utiliza tanto para referirse a las historiatas de origen japonés como al estilo de dibujo utilizado en estas. Abarca una extensa variedad de géneros
+                        y llega a publicos diversos.Tenemos los títulos más famosos y grandiosos del manga como: Dragon Ball, Naruto, One Piece, Death Note de la mano  de los distribuidores oficiales como lo son PANINI  COMICS MEXICO y KAMITE.</p>
+                    <div className="charlotte">
+                        <img src={chivi1} alt="" />
+                    </div>
                 </div>
             </div>
         </>
